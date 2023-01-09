df43196e81d2aad44e39573edce666c3f472e45d
natalievmedrano <nataliemed0403@gmail.com>
2023-01-09 11:25:27 -0600

    modified products.py and imported serializer into products.py

--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def product_list(request):
+    products = Product.objects.all()
 
-    
-    return Response('ok')
+    serializer = ProductSerializer(products, many= True)
+
+    return Response(serializer.data)
